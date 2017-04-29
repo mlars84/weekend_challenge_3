015d840b66fdfb656c5015189490b70742f1fb4d
Matt Larson <matt.a.larson@gmail.com>
2017-04-29 15:03:50 -0500

    more planning and added event listeners

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,2 +1,21 @@
-$( document ).ready();
-console.log('JQ');
+$( document ).ready( onReady );
+console.log( 'JQ' );
+
+function onReady() {
+  console.log( ' in onReady' );
+  $( '#add' ).on( 'click', addTask );
+  $( '#complete' ).on( 'click', completeTask );
+  $( '#delete' ).on( 'click', deleteTask );
+} // end onReady
+
+function addTask() {
+  console.log( 'add button clicked' );
+} // end addTask
+
+function completeTask() {
+  console.log( 'complete button clicked' );
+} // end completeTask
+
+function deleteTask() {
+  console.log( 'delete button clicked' );
+} // end deleteTask
